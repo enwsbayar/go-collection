604cfea27ba59c58f7bb443d84be94c92ef7749b
Enes Bayar <enesbayar04@gmail.com>
2026-06-06 16:47:35 +0300

    Solve nbrOfLaps kata

---
+++ 6kyu/nbr_of_labs.go
@@ -0,0 +1,44 @@
+// Two Joggers
+// Description
+// Bob and Charles are meeting for their weekly jogging tour. They both start at the same spot called "Start" and they each run a different lap, which may (or may not) vary in length. Since they know each other for a long time already, they both run at the exact same speed.
+
+// Illustration
+// Example where Charles (dashed line) runs a shorter lap than Bob:
+
+// Example laps
+
+// Task
+// Your job is to complete the function nbrOfLaps(x, y) that, given the length of the laps for Bob and Charles, finds the number of laps that each jogger has to complete before they meet each other again, at the same time, at the start.
+
+// The function takes two arguments:
+
+// The length of Bob's lap (larger than 0)
+// The length of Charles' lap (larger than 0)
+
+// The function should return an array (Tuple<int, int> in C#) containing exactly two numbers:
+
+// The first number is the number of laps that Bob has to run
+// The second number is the number of laps that Charles has to run
+
+// Examples:
+
+// nbrOfLaps(5, 3); // returns [3, 5]
+// nbrOfLaps(4, 6); // returns [3, 2]
+
+package main
+
+func gcd(a, b int) int {
+    for b != 0 {
+        a, b = b, a%b
+    }
+    return a
+}
+
+func nbrOfLaps(x, y int) []int {
+    if x <= 0 || y <= 0 {
+        return []int{0, 0}
+    }
+
+    lcm := x * y / gcd(x, y)
+    return []int{lcm / x, lcm / y}
+}
