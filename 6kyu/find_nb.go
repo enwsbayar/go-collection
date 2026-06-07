6724de746aabcd1a3c000aad2b4634b87708ea46
Enes Bayar <enesbayar04@gmail.com>
2026-06-07 16:49:24 +0300

    fix: remove unnecessary blank lines in find_nb.go

--- 6kyu/find_nb.go
+++ 6kyu/find_nb.go
@@ -2,12 +2,13 @@
 // n^3, the cube above will have volume of (n-1)^3 and so on until the top which will have a volume of 1^3.
 // You are given the total volume m of the building. Being given m can you find the number n of cubes you will have to build?
 // The function should return n such that n^3 + (n-1)^3 + ... + 1^3 = m, or -1 if no such n exists.
+
 package main
+
 func findNb(m int) int {
     if m <= 0 {
         return -1
     }
-
     target := int64(m)
     sum := int64(0)
     n := 1
